1011b3257ded4f82aefe9efb3afb7cbcbda21ca3
Hubert <hpompecki@gmail.com>
2015-12-20 14:07:50 +0000

    rename #valid_moves to #possible_moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,7 +2,7 @@ module Chess
 
   class Knight < Piece
     
-    def valid_moves(square)
+    def possible_moves(square)
       moves = []
       moves << Piece.offset_square(square, 1, 2)
       moves << Piece.offset_square(square, 2, 1)
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -8,9 +8,9 @@ module Chess
     
     it_behaves_like "a piece"
   
-    describe "#valid_moves" do
+    describe "#possible_moves" do
       it "returns a collection of valid moves" do
-        expect(knight.valid_moves("D5").sort).to eq %w{ E7 F6 F4 E3 C3 B4 B6 C7 }.sort
+        expect(knight.possible_moves("D5").sort).to eq %w{ E7 F6 F4 E3 C3 B4 B6 C7 }.sort
       end
     end
   
