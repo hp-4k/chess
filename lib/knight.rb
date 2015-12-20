ede87cc6068919be02d333f42e5c1a139a6ea673
Hubert <hpompecki@gmail.com>
2015-12-20 12:30:41 +0000

    begin Knight class

---
+++ lib/knight.rb
@@ -0,0 +1,11 @@
+module Chess
+
+  class Knight < Piece
+    
+    def valid_moves(board, square)
+      
+    end
+    
+  end
+
+end
---
+++ spec/knight_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module Chess
+  
+  RSpec.describe Knight do
+  
+    it_behaves_like "a piece"
+  
+  end
+
+end
