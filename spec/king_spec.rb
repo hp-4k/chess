4e9c0856d176f2a23226adeab212d3912c0313ee
Hubert <hpompecki@gmail.com>
2016-01-04 10:46:55 +0000

    fix king_spec to consider castling

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -12,13 +12,13 @@ module Chess
     describe "#possible_moves" do
       context "in the middle of the board" do
         it "returns a collection of possible moves" do
-          expect(king.possible_moves("D5", {}).sort).to eq %w{ C6 D6 E6 E5 E4 D4 C4 C5 }.sort
+          expect(king.possible_moves("D5", {}).sort).to eq %w{ C6 D6 E6 E5 E4 D4 C4 C5 F5 B5 }.sort
         end
       end
       
       context "at the edge of the board" do
         it "returns a collection of possible moves" do
-          expect(king.possible_moves("A3", {}).sort).to eq %w{ A2 B2 B3 B4 A4 }.sort
+          expect(king.possible_moves("A3", {}).sort).to eq %w{ A2 B2 B3 B4 A4 C3 }.sort
         end
       end
     end
